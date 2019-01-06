7552367ab249450df2d3001241a8861b7f937781
Harry Gothold <harrygot@hotmail.com>
2019-01-06 15:36:40 +0000

    added lifecylce hooks

--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { lat: null, errorMessage: '' };
 
+    state = {
+        lat: null,
+        errorMessage: ''
+    }
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => this.setState({ lat: position.coords.latitude }),
             (err) => this.setState({ errorMessage: err.message })
         );
-
     }
 
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
-        return <div>Fetching Location...</div>
+        return <Spinner message='Please Accept Location Request' />
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
