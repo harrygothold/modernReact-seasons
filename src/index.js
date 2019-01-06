c2c185709d2d4e5919e020f96fb01bbc5bef0ca8
Harry Gothold <harrygot@hotmail.com>
2019-01-06 11:36:54 +0000

    added state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { lat: null, errorMessage: '' };
 
-const App = () => {
-    return (
-        <div>This Works</div>
-    )
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
+        );
+
+    }
+
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>latitude: {this.state.lat}</div>
+        }
+        return <div>Fetching Location...</div>
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
